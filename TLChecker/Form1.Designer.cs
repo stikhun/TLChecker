f7c398222739f95037931787a4aee60deb77db3c
ghost1372 <mahdidvb72@gmail.com>
2017-09-24 21:52:43 +0330

    Init Commit

--- TLChecker/Form1.Designer.cs
+++ TLChecker/Form1.Designer.cs
@@ -28,9 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(749, 534);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TLChecker";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
